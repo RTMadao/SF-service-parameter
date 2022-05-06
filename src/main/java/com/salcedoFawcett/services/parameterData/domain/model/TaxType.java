d10a53594e49de7f46169e3827cb0122d4996f11
Carlos Bello <bellocarlos2810@gmail.com>
2022-05-05 23:32:13 -0500

    taxType component added

---
+++ src/main/java/com/salcedoFawcett/services/parameterData/domain/model/TaxType.java
@@ -0,0 +1,23 @@
+package com.salcedoFawcett.services.parameterData.domain.model;
+
+public class TaxType {
+    private String id;
+    private String name;
+    public TaxType(){}
+    public TaxType(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/salcedoFawcett/services/parameterData/web/controller/TaxTypeController.java
@@ -0,0 +1,33 @@
+package com.salcedoFawcett.services.parameterData.web.controller;
+
+import com.salcedoFawcett.services.parameterData.domain.model.TaxType;
+import com.salcedoFawcett.services.parameterData.domain.service.TaxTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/parameter/tax_type")
+public class TaxTypeController {
+    @Autowired
+    private TaxTypeService service;
+    @GetMapping("/")
+    public ResponseEntity<List<TaxType>> getAll(){
+        try {
+            return new ResponseEntity<>(service.getList(), HttpStatus.OK);
+        } catch (IOException e) {
+            try {
+                service.generateJsonFile();
+                return new ResponseEntity<>(service.getList(), HttpStatus.OK);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        }
+    }
+}
